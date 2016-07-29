6f8106363f00cdf18baceffef788f8304367015a
techno-tanoC <develop.tanoc@gmail.com>
2016-07-29 21:43:47 +0900

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,29 @@ module Main where
 
 import Pokemon
 
+selectPokemon :: IO Pokemon
+selectPokemon = do
+  putStrLn "1: Bulbasaur"
+  putStrLn "2: Charmander"
+  putStrLn "3: Squirtle"
+  n <- readLn
+  case n of
+    1 -> return bulbasaur
+    2 -> return charmander
+    3 -> return squirtle
+    _ -> selectPokemon
+
+showDamage :: Move -> Pokemon -> Pokemon -> IO ()
+showDamage move attack defend = do
+  let damage = calc move defend
+      action = pName attack ++ "'s " ++ mName move
+  putStrLn $ action ++ " influcense " ++ show damage ++ " damages!"
+
 main :: IO ()
-main = putStrLn "Welcome to Pokemon World!"
+main = do
+  putStrLn "select your pokemon!"
+  your <- selectPokemon
+  putStrLn "select adversary pokemon!"
+  rival <- selectPokemon
+  let move = moves your !! 0
+  showDamage move your rival
--- src/Pokemon.hs
+++ src/Pokemon.hs
@@ -1,1 +1,46 @@
 module Pokemon where
+
+type Name = String
+type Attack = Type
+type Defend = Type
+type Power = Double
+type Damege = Double
+type Scale = Double
+type Moves = [Move]
+
+data Type = Fire | Water | Grass deriving Show
+data Move = Move {
+    mName :: Name
+  , mType :: Attack
+  , power :: Power
+}
+data Pokemon = Pokemon {
+    pName :: Name
+  , pType :: Defend
+  , moves :: Moves
+}
+
+ember, bubble, vineWhip :: Move
+ember    = Move "Ember" Fire 40
+bubble   = Move "Bubble" Water 40
+vineWhip = Move "Vine Whip" Grass 45
+
+bulbasaur, charmander, squirtle :: Pokemon
+bulbasaur = Pokemon "Bulbasaur" Grass [vineWhip]
+charmander = Pokemon "Charmander" Fire [ember]
+squirtle = Pokemon "Squirtle" Water [bubble]
+
+scale :: Attack -> Defend -> Scale
+scale a d = case (a, d) of
+  (Fire,  Fire)  -> 0.5
+  (Fire,  Water) -> 0.5
+  (Fire,  Grass) -> 2
+  (Water, Fire)  -> 2
+  (Water, Water) -> 0.5
+  (Water, Grass) -> 0.5
+  (Grass, Fire)  -> 0.5
+  (Grass, Water) -> 2
+  (Grass, Grass) -> 0.5
+
+calc :: Move -> Pokemon -> Damege
+calc (Move _ a p) (Pokemon _ d _) = p * scale a d
